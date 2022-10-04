6fcea3c011be3c56fba8d0c6a4c3fe43639149aa
Dmitry Marenich <dimarenich@gmail.com>
2022-10-04 14:00:19 +0200

    Added Login tests

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // env: {
+  //   username: '',
+  //   password: ''
+  // },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://profile.w3schools.com',
+    supportFile: false,
+  },
+};
